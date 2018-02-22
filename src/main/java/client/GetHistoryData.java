57dc75e28e06a3c22924dc06ea4f2e9227654e61
danix <252500748@qq.com>
2018-02-22 16:03:26 +0800

    modified:use HTTPClient to get the History data

--- src/main/java/client/GetHistoryData.java
+++ src/main/java/client/GetHistoryData.java
@@ -1,77 +1,160 @@
 package client;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
 public class GetHistoryData
 {
+    private static String ip = "";
+    private static String port = "";
+    private static String deviceId = "";
+    private static int limit = 1;
+    private static String appName = "";
 
-    private static String url = "http://210.21.223.35:9193/in-cse/360000059447606/AlarmReport";
-    //private static String url = "http://www.baidu.com";
-    private static String param = "cra=20170806T134940&ty=3&lim=1&rcn=1";
+    private static final Logger LOGGER = LoggerFactory.getLogger(GetHistoryData.class);
+    private static ObjectMapper objectMapper = new ObjectMapper();
 
     public static void main(String[] args){
-
-        System.out.print(sendGet(url,param));
+        getRepMsg(ip,port,deviceId,limit,appName);
     }
 
-    public static String sendGet(String url, String param) {
-        StringBuilder result = new StringBuilder();
-        BufferedReader in = null;
-        try {
-            String urlNameString = url + "?" + param;
-            //String urlNameString = url;
-            URL getUrl = new URL(urlNameString);
-            // 打开和URL之间的连接
-            HttpURLConnection connection = (HttpURLConnection)getUrl.openConnection();
-            connection.setRequestMethod("GET");
+    public static Map<String,String> getRepMsg(String ip, String port, String deviceId, Integer limit, String appName){
 
-            // 设置通用的请求属性
-            connection.setRequestProperty("Accept", "application/json");
-            connection.setRequestProperty("X-M2M-RI", "20000000");
-            connection.setRequestProperty("Content-Type","application/x-www-form-urlencoded");
-            connection.setRequestProperty("X-M2M-Origin","/in-cse/qiantanapp");
+        Map<String,String> contents= new LinkedHashMap<>();
 
-            // 建立实际的连接
-            connection.connect();
+        StringBuffer url =new StringBuffer();
+        StringBuffer param = new StringBuffer();
 
-            Map<String, List<String>> map = connection.getHeaderFields();
-            // 遍历所有的响应头字段
-            for (String key : map.keySet()) {
-                System.out.println(key + "--->" + map.get(key));
-            }
+        String currentTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd'T'HHmmss")).toString();
 
+        url.append("http://").append(ip).append(":").append(port).append("/in-cse/").append(deviceId).append("/AlarmReport");
+        param.append("crb=").append(currentTime).append("&ty=3&lim=").append(limit).append("&rcn=1");
 
+        String resp = sendGetByClient(url.toString(),param.toString(),appName);
+        if (resp != "")
+        {
+            try
+            {
+                LOGGER.debug(resp);
+                JsonNode node = objectMapper.readTree(resp);
+//                int size = node.get("m2m:agr").get("m2m:rsp").get("pc").get("m2m:cin").size();
+                Iterator<JsonNode> iterator = node.get("m2m:agr").get("m2m:rsp").get("pc").get("m2m:cin").iterator();
+                while (iterator.hasNext())
+                {
+                    JsonNode jsonNode = iterator.next();
+                    String time = jsonNode.get("lt").asText();
+                    String content = jsonNode.get("con").asText();
+                    content = hexToString(content);
 
-
-            // 定义 BufferedReader输入流来读取URL的响应
-            in = new BufferedReader(new InputStreamReader(
-                    connection.getInputStream(),"UTF-8"));
-            String line;
-            while ((line = in.readLine()) != null) {
-                result.append(line);
-            }
-
-            connection.disconnect();
-
-        } catch (Exception e) {
-            System.out.println("发送GET请求出现异常！" + e);
-            e.printStackTrace();
-        }
-        // 使用finally块来关闭输入流
-        finally {
-            try {
-                if (in != null) {
-                    in.close();
+                    contents.put(time, content);
                 }
-            } catch (Exception e2) {
-                e2.printStackTrace();
+
+                LOGGER.debug("size : " + contents.size());
+
+            } catch (IOException e)
+            {
+                e.printStackTrace();
             }
         }
-        return result.toString();
+
+        return contents;
     }
+
+    public static String sendGetByClient(String url, String param,String appName){
+        String result = "";
+        String urlNameString = url + "?" + param;
+
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+
+        try
+        {
+            HttpGet request = new HttpGet(urlNameString);
+            request.setHeader("Accept", "application/json");
+            request.setHeader("X-M2M-RI", "20000000");
+            request.setHeader("Content-Type","application/x-www-form-urlencoded");
+            request.setHeader("Content-Length","0");
+            request.setHeader("X-M2M-Origin","/in-cse/"+appName);
+
+
+            CloseableHttpResponse response = httpClient.execute(request);
+            LOGGER.debug(String.valueOf(response.getStatusLine()));
+            try
+            {
+                HttpEntity entity = response.getEntity();
+                if (entity != null){
+                    result = EntityUtils.toString(entity);
+                }
+            } finally
+            {
+                response.close();
+            }
+
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }finally
+        {
+            try
+            {
+                httpClient.close();
+            } catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+        return result;
+    }
+
+    // 转化十六进制编码为字符串
+    private static String hexToString(String s)
+    {
+        if (s== null || s.equals("")){
+            return null;
+        }
+        s=s.replace("\"","");
+        s= s.replace(" ","");
+
+        byte[] baKeyword = new byte[s.length()/2];
+        for(int i = 0; i < baKeyword.length; i++)
+        {
+            try
+            {
+                baKeyword[i] = (byte)(0xff & Integer.parseInt(s.substring(i*2, i*2+2),16));
+            }
+            catch(Exception e)
+            {
+                LOGGER.warn("HexString Error");
+            }
+        }
+        try
+        {
+            s = new String(baKeyword, "utf-8");
+        }
+        catch (Exception e1)
+        {
+            LOGGER.warn("Hex to String Error");
+        }
+        return s;
+    }
+
 }
